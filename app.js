9159b3f50269cd3812a0a29b6b769985f106e0ce
ejoreo <ejorear@gmail.com>
2017-04-18 14:50:08 -0700

    Redis client ES6 refactor

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var redis = require('redis');
-var redisClient = redis.createClient({host : 'localhost', port : 6379});
+const redis = require('redis');
+const redisClient = redis.createClient({host : 'localhost', port : 6379});
 
-redisClient.on('ready',function() {
- console.log("Redis is ready");
+redisClient.on('ready', () => {
+ console.log("Redis is connected");
 });
 
-redisClient.on('error',function() {
+redisClient.on('error', () => {
  console.log("Error in Redis");
 });
+
+// auth example
+redisClient.auth('password', (err,reply) => {
+ console.log(reply);
+});
+
+
+// setting the 'language' key with the value 'nodejs'
+redisClient.set("language","nodejs");
+
+// getting data from the redis client
+redisClient.get("language", (err,reply) => {
+ console.log(err);
+ console.log(reply);
+})
